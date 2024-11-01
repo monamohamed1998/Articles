4a4eb638f654dbd685c15fc9ab0425f690acb41c
mona <moona944198@gmai.com>
2024-11-01 14:09:18 +0200

    the api base

--- lib/api_manager.dart
+++ lib/api_manager.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:newsapp2/api_constants.dart';
-import 'package:newsapp2/model/news_response/news_response.dart';
+import 'package:newsapp2/model/news_response/source_response.dart';
 import 'package:newsapp2/model/news_response/source.dart';
 
 // https://newsapi.org/v2/top-headlines/sources?apiKey=c319f610e68f4f4bb4e1ef371b44df61
